99164a4202596cf9152b3d99939d0ae111c07f06
Vadim <vadlen2149@gmail.com>
2022-03-24 14:09:14 +0300

    homework-3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { AttributeDirective } from './attribute.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    AttributeDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/attribute.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, Input, ElementRef, OnChanges } from '@angular/core';
+
+@Directive({
+    selector: '[appAttribute]',
+  })
+
+export class AttributeDirective implements OnChanges {
+    @Input()
+
+    public appAttribute: object;
+
+    constructor(private element: ElementRef<HTMLElement>) {}
+
+    public ngOnChanges() {
+      for (let key in this.appAttribute) {
+        this.element.nativeElement.setAttribute(key, this.appAttribute[key]);
+      }
+    }
+}
